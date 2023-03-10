28a7ed043aa13208ca4466db272e6fdcba7a8414
a7med3ouda <oudacode@gmail.com>
2023-03-10 20:28:13 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { generateId } from './@types/static';
+import { TypingService } from './services/typing.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ai-chat';
+  title = 'ai-bot';
+  responses = [];
+  isLoading = false;
+  chatList = [];
+
+  constructor(public typing: TypingService) {
+    setTimeout(() => {
+      typing.make('hello my friend .. how are you doing?');
+      console.log(generateId());
+      console.log(generateId());
+      console.log(generateId());
+      console.log(generateId());
+      console.log(generateId().length);
+    }, 3000);
+  }
+
+  handleSendMessage(e: any) {
+    e.preventDefault();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SendIconComponent } from './components/send-icon/send-icon.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { BotIconComponent } from './components/bot-icon/bot-icon.component';
+import { UserIconComponent } from './components/user-icon/user-icon.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SendIconComponent, LoadingComponent, BotIconComponent, UserIconComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
