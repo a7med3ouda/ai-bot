b7a3ddcefe932c926ab3b6fc919818838632112b
a7med3ouda <oudacode@gmail.com>
2023-03-10 20:33:10 +0200

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SendIconComponent } from './components/send-icon/send-icon.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { BotIconComponent } from './components/bot-icon/bot-icon.component';
 import { UserIconComponent } from './components/user-icon/user-icon.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, SendIconComponent, LoadingComponent, BotIconComponent, UserIconComponent],
+  declarations: [
+    AppComponent,
+    SendIconComponent,
+    LoadingComponent,
+    BotIconComponent,
+    UserIconComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
